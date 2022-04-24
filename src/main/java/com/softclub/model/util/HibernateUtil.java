caebc686e549721161d30bb618c8b05fc1a8d43e
”Mikhail <ryzgunsky.mihail@gmail.com>
2022-04-24 23:04:02 +0300

    add util classes

---
+++ src/main/java/com/softclub/model/util/HibernateUtil.java
@@ -0,0 +1,23 @@
+package com.softclub.model.util;
+
+import com.softclub.model.entity.coffee.Coffee;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        Configuration configuration = new Configuration().configure();
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
